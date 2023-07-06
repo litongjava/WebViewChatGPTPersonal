7a88a882a8414a8e136376d55386fb481ecf6347
litongjava <litongjava@qq.com>
2023-07-07 06:26:41 +0800

    init commit

---
+++ app/src/main/java/com/liotngjava/web/view/chatgpt/personal/MainActivity.java
@@ -0,0 +1,36 @@
+package com.liotngjava.web.view.chatgpt.personal;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
+
+import com.liotngjava.web.view.chatgpt.personal.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+  private WebView myWebView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    getSupportActionBar().hide(); //隐藏ActionBar
+    setContentView(R.layout.activity_main);
+
+    String url = "https://chat.openai.com/";
+    CustomTabsIntent intent = new CustomTabsIntent.Builder()
+      .setUrlBarHidingEnabled(true)
+      .build();
+    intent.launchUrl(MainActivity.this, Uri.parse(url));
+  }
+  @Override
+  protected void onResume() {
+    super.onResume();
+    // 在这里调用 finish() 来结束您的应用程序
+    finish();
+  }
+}
